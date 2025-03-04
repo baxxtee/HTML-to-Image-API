ca3e7848dfee60f2e1d2feeb43b11babe0929dba
baxxtee <104707778+baxxtee@users.noreply.github.com>
2025-03-04 04:35:40 +0300

    app.js

--- app.js
+++ app.js
@@ -56,4 +56,4 @@ app.use(express.json());
 
 // listen to port
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Server started and running on port ' + PORT));
+app.listen(PORT, '0.0.0.0', () => console.log('Server started and running on port ' + PORT));
